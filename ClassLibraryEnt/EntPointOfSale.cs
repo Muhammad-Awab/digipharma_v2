82d1ea773268b7c9fc6ee0fc6c586ebfc88f5407
rehan985 <rehan.akhtar.985@gmail.com>
2023-10-15 20:33:37 +0500

    Add all Entities in ENT layer

---
+++ ClassLibraryEnt/EntPointOfSale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryEnt
+{
+    public class EntPointOfSale
+    {
+        public int pk_PointOfSaleId { get; set; }
+        public int fk_BillingId { get; set; }
+        public int fk_UserId { get; set; }
+        public int fk_MedicineId { get; set; }
+        public string CustomerName { get; set; }
+        public string CurrentDate { get; set; }
+        public string Quantity { get; set; }
+        public string TotalPrice { get; set; }
+
+    }
+}
