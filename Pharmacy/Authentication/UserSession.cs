22c5da2cf8062590aebe1d909e3346285138e8fe
Muhammad-Awab <muhammadawab82002@gmail.com>
2023-12-16 19:59:07 +0500

    Authorization and Authentication complete UserLogin Img Add Aws s3 Api Add

--- Pharmacy/Authentication/UserSession.cs
+++ Pharmacy/Authentication/UserSession.cs
@@ -7,6 +7,7 @@
         public string? UserId { get; set; }
         public string? PharmacyId { get; set; }
         public string? PharmacyName { get; set; }
+        public string? imageUrl { get; set; }
 
     }
 }
