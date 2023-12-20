6b78e42fc83528eef25e6bd6ed895e73ed18d13e
rehan985 <rehan.akhtar.985@gmail.com>
2023-12-20 21:57:58 +0500

    add total sales record row and multiple databse attachment done.

--- ClassLibraryDAL/DBHelper.cs
+++ ClassLibraryDAL/DBHelper.cs
@@ -11,9 +11,36 @@ namespace ClassLibraryDAL
     {
         public static SqlConnection GetConnection()
         {
+            string[] connectionStrings = {
+                @"Data Source=.;Initial Catalog=db_pharmacy;Integrated Security=True",
+                @"Data Source=.;Initial Catalog=db_pharmacy;Integrated Security=True" // Add your second connection string here
+            };
 
-            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=db_pharmacy;Integrated Security=True");
+            SqlConnection con = null;
 
+            foreach (string connectionString in connectionStrings)
+            {
+                try
+                {
+                    con = new SqlConnection(connectionString);
+                    con.Open();
+                    // If connection is successful, break the loop
+                    break;
+                }
+                catch (SqlException)
+                {
+                    // Handle exceptions or try the next connection string
+                    con?.Close();
+                    con?.Dispose();
+                    con = null;
+                }
+            }
+
+            if (con == null)
+            {
+                throw new Exception("Failed to establish a connection to the database.");
+            }
+            con.Close();
             return con;
         }
     }
