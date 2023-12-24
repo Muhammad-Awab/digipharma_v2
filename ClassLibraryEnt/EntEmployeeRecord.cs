9a14ed4f7e57f19fd386c62e48955cb00f4302ca
rehan985 <rehan.akhtar.985@gmail.com>
2023-12-24 21:28:54 +0500

    validation in medicine record and login form

--- ClassLibraryEnt/EntEmployeeRecord.cs
+++ ClassLibraryEnt/EntEmployeeRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,38 @@ namespace ClassLibraryEnt
     public class EntEmployeeRecord
     {
         public int fk_PharmacyId { get; set; }
+        [Required]
         public string? UserFullName { get; set; }
+        [Required]
+           
         public string? PhoneNumber { get; set; }
+        [Required]
+
         public string? Address { get; set; }
+        [Required]
+
         public string? WorkExperience { get; set; }
+        [Required]
+
         public string? PharmacyName { get; set; }
+        [Required]
+
         public string? Qualification { get; set; }
+        [Required]
+
         public DateTime? DateOfJoining { get; set; }
+       
+
         public string? Username { get; set; }
+       
+
         public string? Password { get; set; }
+       
+
         public string? Role{ get; set; }
+        
+
         public string? UserImg { get; set; }
+
     }
 }
